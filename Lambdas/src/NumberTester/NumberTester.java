b9fb97799194ba3c0b06a1b1aca8bbcaf8369681
StarterLib <LangF21008@sus.htl-grieskirchen.at>
2024-09-24 12:11:49 +0200

    added scanner for csv file in NumberTester

--- Lambdas/src/NumberTester/NumberTester.java
+++ Lambdas/src/NumberTester/NumberTester.java
@@ -1,16 +1,48 @@
 package NumberTester;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class NumberTester implements NumberTest {
 
    private NumberTest oddTester;
    private NumberTest primeTester;
    private NumberTest palindromeTester;
 
+   //---------------------------------------
+   List<Integer> firstNumberList= new ArrayList<>();
+   List<Integer> secondNumberList= new ArrayList<>();
 
 
 
    public NumberTester(String fileName){
       fileName="text.csv";
+
+      try {
+         Scanner scanner = new Scanner(new File(fileName));
+         scanner.useDelimiter("\n");
+         while (scanner.hasNext()) {
+            String s = scanner.next();
+            String[] numbers = s.split(" ");
+            for (int i = 0; i <numbers.length; i++) {
+
+               if (i % 2 ==0){
+                  firstNumberList.add(Integer.valueOf(numbers[i]));
+               }
+
+               if (i % 2 ==1){
+                  secondNumberList.add(Integer.valueOf(numbers[i]));
+               }
+            }
+         }
+
+      } catch (FileNotFoundException ex) {
+
+      }
+
    }
 
    public void setOddEvenTester(NumberTest oddTester){
@@ -32,6 +64,9 @@ public class NumberTester implements NumberTest {
 
    @Override
    public boolean testNumber(int number) {
-      return false;
+         return false;
    }
+
+
+
 }
