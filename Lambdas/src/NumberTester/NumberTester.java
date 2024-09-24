2781bb4c76248f13cf8f4c4c7f10f20ddf423ec7
StarterLib <LangF21008@sus.htl-grieskirchen.at>
2024-09-24 11:30:00 +0200

    created setters

--- Lambdas/src/NumberTester/NumberTester.java
+++ Lambdas/src/NumberTester/NumberTester.java
@@ -1,13 +1,35 @@
 package NumberTester;
 
-public class NumberTester {
+public class NumberTester implements NumberTest {
+
+   private NumberTest oddTester;
+   private NumberTest primeTester;
+   private NumberTest palindromeTester;
+
 
    public NumberTester(String fileName){
 
    }
 
+   public void setOddEvenTester(NumberTest oddTester){
+      this.oddTester=oddTester;
+   }
+
+   public void setPrimeTester(NumberTest primeTester){
+      this.primeTester= primeTester;
+   }
+
+   public void setPalindromeTester(NumberTest palindromeTester){
+      this.palindromeTester= palindromeTester;
+   }
+
+   public void testFile(){
+
+   }
 
 
-
-
+   @Override
+   public boolean testNumber(int number) {
+      return false;
+   }
 }
