91ae6efbbfd06bf1a3af2ce009b788f5a3fee8e2
StarterLib <LangF21008@sus.htl-grieskirchen.at>
2024-09-30 17:11:35 +0200

    Created and implemented abstract Class AbstractCalculator

---
+++ Lambdas/src/calculations/AbstractCalculator.java
@@ -0,0 +1,27 @@
+package calculations;
+
+public abstract class AbstractCalculator {
+    protected CalculationOperation add;
+    protected CalculationOperation subtract;
+    protected CalculationOperation multiply;
+    protected CalculationOperation divide;
+
+    public AbstractCalculator(CalculationOperation add,
+                              CalculationOperation subtract,
+                              CalculationOperation multiply,
+                              CalculationOperation divide) {
+        this.add = add;
+        this.subtract = subtract;
+        this.multiply = multiply;
+        this.divide = divide;
+    }
+
+    public abstract Number add(Number a, Number b);
+
+    public abstract Number subtract(Number a, Number b);
+
+    public abstract Number multiply(Number a, Number b);
+
+    public abstract Number divide(Number a, Number b);
+}
+
